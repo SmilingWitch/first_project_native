3370852c7bddce660a16a585d3434b319897e071
SmilingWitch <danielitayero@gmail.com>
2024-06-18 22:05:22 -0300

    feat: add data to repositories.js

--- src/data/repositories.js
+++ src/data/repositories.js
@@ -9,5 +9,60 @@ export default [
         ratingAverage: 0,
         reviewCount: 0,
         ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4'
-    }
+    },
+    {
+        id: 'midudev.midu.dev1',
+        fullName: 'midudev/midu.dev',
+        description: 'Blog de noticias sobre desarrollo',
+        language: 'HTML',
+        forksCount: 3123,
+        stargazersCount: 52869,
+        ratingAverage: 0,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4'
+    },
+    {
+        id: 'midudev.midu.dev2',
+        fullName: 'midudev/midu.dev',
+        description: 'Blog de noticias sobre desarrollo',
+        language: 'HTML',
+        forksCount: 3123,
+        stargazersCount: 52869,
+        ratingAverage: 0,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4'
+    },
+    {
+        id: 'midudev.midu.dev3',
+        fullName: 'midudev/midu.dev',
+        description: 'Blog de noticias sobre desarrollo',
+        language: 'HTML',
+        forksCount: 3123,
+        stargazersCount: 52869,
+        ratingAverage: 0,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4'
+    },
+    {
+        id: 'midudev.midu.dev4',
+        fullName: 'midudev/midu.dev',
+        description: 'Blog de noticias sobre desarrollo',
+        language: 'HTML',
+        forksCount: 3123,
+        stargazersCount: 52869,
+        ratingAverage: 0,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4'
+    },
+    {
+        id: 'midudev.midu.dev5',
+        fullName: 'midudev/midu.dev',
+        description: 'Blog de noticias sobre desarrollo',
+        language: 'HTML',
+        forksCount: 3123,
+        stargazersCount: 52869,
+        ratingAverage: 0,
+        reviewCount: 0,
+        ownerAvatarUrl: 'https://avatars.githubusercontent.com/u/1561955?v=4'
+    },
 ]
