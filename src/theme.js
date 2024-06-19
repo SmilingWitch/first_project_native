7493d4d726c3199eea471c85c6c891e149373597
SmilingWitch <danielitayero@gmail.com>
2024-06-18 22:02:35 -0300

    feat: add theme.js

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+
+const theme = {
+    appBar: {
+        primary: '#24292e',
+        textPrimary: '#fff',
+        textSencundary: '#999'
+    },
+    colors: {
+        textPrimary: '#24292e',
+        textSecundary: '#586069',
+        primary: '#0366d6',
+        white: "#fefefe"
+    },
+    fontSize: {
+        body: 14,
+        subheading: 16
+    },
+    fonts: {
+        main: 'System'
+    },
+    fontWeight: {
+        normal: '400',
+        bold: '700'
+    }
+}
+
+export default theme;
