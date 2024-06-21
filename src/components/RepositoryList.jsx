af9be6558834313cec9d31a24d95f2fb04e7af18
SmilingWitch <danielitayero@gmail.com>
2024-06-21 15:08:46 -0300

    feat: add repositories backend petitions to RepositoryList

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,9 +1,13 @@
 import {Text, FlatList} from "react-native"
-import repositories from "../data/repositories"
 import RepositoryItem from "./RepositoryItme";
-
+import { useEffect, useState } from "react";
+import useRepositories from "../hooks/useRepositories";
 
 const RepositoryList = () => {
+
+    const {repositories} = useRepositories()
+    
+
     return(
 
         <FlatList data = {repositories}
